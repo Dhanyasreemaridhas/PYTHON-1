fc6a4cf4010a1dbe422f9e1e22a68c2450b292ef
Dhanya Sree <dhanyasreed1505@gmail.com>
2025-06-15 08:55:49 +0530

    Created pythonintro.py

---
+++ pythonintro.py
@@ -0,0 +1,55 @@
+print("Hello World")
+print("###")
+print(2495+789358)
+print(193856-596)
+
+a=37
+b=61
+c=391
+d=a*b*c
+print(d)
+
+a=33968
+b=176
+c=a/b
+print(c)
+
+word=input()
+print(word)
+
+word=input()
+print(word)
+print("###")
+
+word1=input()
+word2=input()
+print(word1)
+print(word2)
+
+word1=input()
+word2=input()
+print(word2)
+
+word1=input()
+word2=input()
+print(word2)
+print(word1)
+
+word1=input()
+print("Hello "+word1)
+
+word1=input()
+print("Given input: "+word1)
+
+word1=input()
+word2=input()
+print(word1+word2)
+
+firstname=input()
+lastname=input()
+print(firstname+" "+lastname)
+
+name=input()
+age=input()
+print(name+" is "+age+" years old")
+
